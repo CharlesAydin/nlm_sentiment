561bb52938c1fb9571d077d7760dbd6404ecee4f
sahilzubair <zubairs@vcu.edu>
2016-03-29 23:14:46 -0400

    g

--- graph.py
+++ graph.py
@@ -22,7 +22,7 @@ numwords = np.max(train_in)+1
 graph = Graph()
 
 graph.add_input(name='emotion_vector', input_shape=((1,)), dtype='int')
-graph.add_node(Embedding(4096, 2, input_length=1), name='emotion_embeddings', input='emotion_vector')
+graph.add_node(Embedding(8096, 2, input_length=1), name='emotion_embeddings', input='emotion_vector')
 graph.add_node(Flatten(), input='emotion_embeddings', name='flatemot')
 
 
@@ -40,7 +40,7 @@ graph.add_node(Dense(numwords), input='a22a', name='out')
 graph.add_node(Activation('softmax'), input='out', name='softmax')
 graph.add_output(name='output', input='softmax')
 
-a = Adam(lr=0.0001)
+a = Adam(lr=0.001)
 graph.compile(optimizer=a, loss={'output':'categorical_crossentropy'})
 
 for e in range(0, 50):
